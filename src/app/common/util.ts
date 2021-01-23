e4c7ff144a4162986a4c821ac77ae51265b79e03
Deva <devaprakash.j@idp.com>
2021-01-23 14:22:16 +0530

    first commit

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,6 +1,6 @@
 export class Util {
 
-    CF_DELIVERY_API = 'http://cdn.contentful.com/spaces/i9f438s6ez2a/environments/master/entries/';
+    CF_DELIVERY_API = 'https://cdn.contentful.com/spaces/i9f438s6ez2a/environments/master/entries/';
     CF_DELIVERY_ACCESS_KEY = '8An7wVkx6PFqqUMe-wpPcIlHihHCHpGkCA-0plecmf0';
     HOME_CONTENT_TYPE = 'homePage';
     HEADER_CONTENT_TYPE = 'header';
