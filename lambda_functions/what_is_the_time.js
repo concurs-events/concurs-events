cdccbcfa8d051d1153bc258bdfa2822561499e8e
Deva <devaprakash.j@idp.com>
2021-04-08 17:57:07 +0530

    function

--- lambda_functions/what_is_the_time.js
+++ lambda_functions/what_is_the_time.js
@@ -1,7 +1,9 @@
 module.exports.handler = async (event, context) => {
     const unixTime = Math.floor(Date.now() / 1000);
+    console.log(event)
+    console.log(context)
     return {
         statusCode: 200,
-        body: event,
+        body: unixTime,
     };
 };
