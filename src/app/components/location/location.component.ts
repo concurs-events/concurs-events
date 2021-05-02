faa35d2523bd1f8f3cabb32e251436b465514801
Deva <devaprakash.j@idp.com>
2021-05-02 14:31:09 +0530

    responsive

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -11,13 +11,15 @@ export class LocationComponent implements OnInit {
 
   @Input() contactDetails
   public iframe
+  public iframeStyle = ' style="width: 100%;position: absolute;top: 0;left: 0;bottom: 0;right: 0;width: 100%;height: 100%;border: 0;"'
 
   constructor(protected _sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
-    console.log('details', this.contactDetails)
-    this.iframe = this._sanitizer.bypassSecurityTrustHtml(this.contactDetails.loction)
+    let iframe = this.contactDetails.loction
+    iframe = iframe.substring(0, iframe.indexOf("src=")) + this.iframeStyle + iframe.substring(iframe.indexOf("src="))
+    this.iframe = this._sanitizer.bypassSecurityTrustHtml(iframe)
   }
 
   getIframeUrl(location) {
